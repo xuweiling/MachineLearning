d0961e2690d9052ce7b45c063524c2a4a3ea476c
xuweiling <welinxu@gmail.com>
2017-04-09 18:22:13 +0800

    Baike Crawl

---
+++ html_downloader.py
@@ -0,0 +1,12 @@
+#coding:utf8
+import urllib2
+
+class HtnlDownloader(object):
+    def download(self, url):
+        if url is None:
+            return None
+        response = urllib2.urlopen(url)
+
+        if response.getcode()!=200:
+            return
+        return response.read()
